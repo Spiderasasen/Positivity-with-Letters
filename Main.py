955217306789b37c657a1d7741bf6ebfe9ee1fa7
Diego-Diaz797 <ddiaz11@elon.edu>
2025-08-28 13:16:50 -0400

    Calling the main Text file and seeing if it works
    We are just calling the text file and seeing if the text file works

--- Main.py
+++ Main.py
@@ -1,2 +1,18 @@
 # calling the main text file
-print('Hello World!')
+def main():
+    #Checking if the code was found
+    try:
+        with open("Positive-Words.txt") as f:
+            lines = f.readlines()
+            words = [line.strip() for line in lines]
+
+        print(words)
+    #   If the file was not found
+    except FileNotFoundError:
+        print(f"Error: File Not Found.")
+    # If there was a differnt code
+    except Exception as e:
+        print(f"Error Occurred {e}")
+
+if __name__ == "__main__":
+    main()
